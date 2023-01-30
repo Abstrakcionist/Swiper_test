75cba604387b09e616c9e7fd8d71df01ddd72278
Abstrakcionist <kondratev04g@mail.ru>
2023-01-30 21:48:26 +0500

    доработаные эффекты

--- swiper_script.js
+++ swiper_script.js
@@ -1,6 +1,8 @@
 const swiper = new Swiper('.swiper', {
-    speed: 400,
+    speed: 800,
     spaceBetween: 100,
+    loop: true,
+    grabCursor: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
